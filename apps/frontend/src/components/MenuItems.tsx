d8d4fad1849aef037e084df368a91457315fbfe8
Sid <siddharthmethiya180@gmail.com>
2025-08-02 01:24:46 +0530

    Completed login page and sidebar

---
+++ apps/frontend/src/components/MenuItems.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom'
+import { menuItemsData } from '../assets/assets'
+import type { SidebarProps } from './Sidebar'
+
+export const MenuItems = ({ setSideBarOpen }: SidebarProps) => {
+   return (
+      <div className="px-6 text-gray-600 space-y-1 font-medium">
+         {menuItemsData.map(({ to, label, Icon }) => (
+            <NavLink
+               key={to}
+               to={to}
+               end={to === '/'}
+               onClick={() => {
+                  setSideBarOpen(false)
+               }}
+               className={({ isActive }) =>
+                  `px-3.5 py-2 flex items-center gap-3 rounded-xl ${isActive ? 'bg-indigo-50 text-indigo-700' : 'hover:bg-gray-50'}`
+               }
+            >
+               <Icon className="w-5 h-5" />
+               {label}
+            </NavLink>
+         ))}
+      </div>
+   )
+}
--- apps/frontend/src/pages/Layout.tsx
+++ apps/frontend/src/pages/Layout.tsx
@@ -1,3 +1,36 @@
+import { Outlet } from 'react-router-dom'
+import { Sidebar } from '../components/Sidebar'
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
+import { dummyUserData } from '../assets/assets'
+import { Loading } from '../components/Loading'
+
 export const Layout = () => {
-   return <div></div>
+   const user = dummyUserData
+   const [sidebar, setSideBarOpen] = useState(false)
+   return user ? (
+      <div className="w-full h-screen flex">
+         <Sidebar sidebarOpen={sidebar} setSideBarOpen={setSideBarOpen} />
+         <div className="flex-1 bg-slate-50">
+            <Outlet />
+         </div>
+         {sidebar ? (
+            <X
+               className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+               onClick={() => {
+                  setSideBarOpen(false)
+               }}
+            />
+         ) : (
+            <Menu
+               className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+               onClick={() => {
+                  setSideBarOpen(true)
+               }}
+            />
+         )}
+      </div>
+   ) : (
+      <Loading />
+   )
 }
