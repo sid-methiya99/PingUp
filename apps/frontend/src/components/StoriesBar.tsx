a8fa793b7b498f011da0b2aa3332f41e460328a2
Sid <siddharthmethiya180@gmail.com>
2025-08-03 23:00:21 +0530

    Added story Modal

--- apps/frontend/src/components/StoriesBar.tsx
+++ apps/frontend/src/components/StoriesBar.tsx
@@ -2,9 +2,13 @@ import { useEffect, useState } from 'react'
 import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
 import moment from 'moment'
+import { StoryModal } from './StoryModal'
 
 export const StoriesBar = (props: {}) => {
    const [stories, setStories] = useState<(typeof dummyStoriesData)[]>([])
+   const [showModal, setShowModal] = useState(false)
+   const [viewStory, setViewStory] = useState(false)
+
    const fetchStories = async () => {
       setStories([dummyStoriesData])
    }
@@ -17,6 +21,9 @@ export const StoriesBar = (props: {}) => {
          <div className="flex gap-4 pb-5">
             {/* Add Story Card */}
             <div
+               onClick={() => {
+                  setShowModal(true)
+               }}
                className="rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg 
             transition-all duration-100 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white"
             >
@@ -48,11 +55,33 @@ export const StoriesBar = (props: {}) => {
                         <p className="text-white absolute bottom-1 right-2 z-10 text-xs">
                            {moment(x.createdAt).fromNow()}
                         </p>
+                        {x.media_type != 'text' && (
+                           <div className="absolute inset-0 z-1 rounded-lg bg-black overflow-hidden">
+                              {x.media_type === 'image' ? (
+                                 <img
+                                    src={x.media_url}
+                                    className="h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80"
+                                 />
+                              ) : (
+                                 <video
+                                    src={x.media_url}
+                                    className="h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80"
+                                 />
+                              )}
+                           </div>
+                        )}
                      </div>
                   ))}
                </div>
             ))}
          </div>
+         {/* Add story modal */}
+         {showModal && (
+            <StoryModal
+               setShowModal={setShowModal}
+               fetchStories={fetchStories}
+            />
+         )}
       </div>
    )
 }
---
+++ apps/frontend/src/components/StoryModal.tsx
@@ -0,0 +1,119 @@
+import { ArrowLeft, Sparkle, TextIcon, Upload } from 'lucide-react'
+import { use, useState } from 'react'
+
+export const StoryModal = ({ setShowModal, fetchStories }) => {
+   const bgcolor = [
+      '#4f46e5',
+      '#7c3aed',
+      '#db2777',
+      '#e11d48',
+      '#ca8a04',
+      '#0d9488',
+   ]
+   const [mode, setMode] = useState('text')
+   const [background, setBackground] = useState(bgcolor[0])
+   const [text, setText] = useState('')
+   const [media, setMedia] = useState('')
+   const [previewUrl, setPreviewUrl] = useState('')
+
+   const handleMediaUpload = (e) => {
+      const file = e.target.files?.[0]
+      if (file) {
+         setMedia(file)
+         setPreviewUrl(URL.createObjectURL(file))
+      }
+   }
+
+   const handleCreateStory = async (e) => {}
+   return (
+      <div className="fixed inset-0 z-110 min-h-screen bg-black/80 backdrop-blur text-white flex items-center justify-center p-4">
+         <div className="w-full max-w-md">
+            <div className="text-center mb-4 flex items-center justify-between">
+               <button
+                  onClick={() => {
+                     setShowModal(false)
+                  }}
+                  className="text-center mb-4 flex items-center justify-center"
+               >
+                  <ArrowLeft />
+               </button>
+               <h2 className="text-lg font-semibold">Create Story</h2>
+               <span className="w-10 "></span>
+            </div>
+
+            <div
+               className="rounded-lg h-96 flex items-center justify-center relative border"
+               style={{
+                  backgroundColor: background,
+               }}
+            >
+               {mode === 'text' && (
+                  <textarea
+                     className="bg-transparent text-white w-full h-full p-6 text-lg resize-none 
+                     focus:outline-none"
+                     placeholder="What's on your mind?"
+                     onChange={(e) => setText(e.target.value)}
+                     value={text}
+                  />
+               )}
+               {mode === 'media' &&
+                  previewUrl &&
+                  (media?.type.startsWith('image') ? (
+                     <img
+                        src={previewUrl}
+                        className="object-contain max-h-full border"
+                     />
+                  ) : (
+                     <video
+                        src={previewUrl}
+                        className="object-contain max-h-full"
+                     />
+                  ))}
+            </div>
+            <div className="flex mt-4 gap-2 ">
+               {bgcolor.map((x) => (
+                  <button
+                     key={x}
+                     className="w-6 h-6 rounded-full ring cursor-pointer bg-black"
+                     style={{
+                        backgroundColor: x,
+                     }}
+                     onClick={() => {
+                        setBackground(x)
+                     }}
+                  />
+               ))}
+            </div>
+            <div className="flex mt-4 gap-2 ">
+               <button
+                  onClick={() => {
+                     setMode('text')
+                     setMedia('')
+                     setPreviewUrl('')
+                  }}
+                  className={`flex-1 flex items-center justify-center gap-2 p-2 cursor-pointer rounded ${mode === 'text' ? 'bg-white text-black' : 'bg-zinc-800'}`}
+               >
+                  <TextIcon size={18} /> Text
+               </button>
+               <label
+                  className={`flex-1 flex items-center justify-center gap-2 p-2 rounded cursor-pointer ${mode === 'media' ? 'bg-white text-black' : 'bg-zinc-800'}`}
+               >
+                  <input
+                     onChange={(e) => {
+                        handleMediaUpload(e)
+                        setMode('media')
+                     }}
+                     type="file"
+                     accept="image/*, video/*"
+                     className="hidden"
+                  />
+                  <Upload size={18} /> Photo/Video
+               </label>
+            </div>
+            <button className="flex items-center w-full justify-center gap-2 py-2.5  mt-6 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 text-white cursor-pointer">
+               <Sparkle size={18} /> Create Story
+            </button>
+         </div>
+      </div>
+   )
+}
